8d27022e3fa186fa8a14a9bb12a9860f1dbc1670
BD <bd002@nym.hush.com>
2018-06-29 22:02:22 -0500

    added property

--- src/main/java/bdn/helloworld/repository/AccountRepositoryImpl.java
+++ src/main/java/bdn/helloworld/repository/AccountRepositoryImpl.java
@@ -3,6 +3,7 @@ package bdn.helloworld.repository;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 import bdn.helloworld.model.FinSecurity;
@@ -10,11 +11,15 @@ import bdn.helloworld.model.FinSecurity;
 @Repository("accountRepository")
 public class AccountRepositoryImpl implements AccountRepository {
 
+	@Value("${dbUsername}")
+	String dbUsername;
+	
 	/* (non-Javadoc)
 	 * @see bdn.helloworld.repository.AccountRepository#getAllFinSecurities()
 	 */
 	@Override
 	public List<FinSecurity> getAllFinSecurities() {
+		System.out.println("dbUsername = " + dbUsername);
 		List<FinSecurity> _list = new ArrayList<FinSecurity>();
 		
 		FinSecurity _security = new FinSecurity();
--- src/main/java/bdn/helloworld/service/AccountServiceImpl.java
+++ src/main/java/bdn/helloworld/service/AccountServiceImpl.java
@@ -4,12 +4,15 @@ import java.util.List;
 import java.util.ListIterator;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 
 import bdn.helloworld.model.FinSecurity;
 import bdn.helloworld.repository.AccountRepository;
 
 @Service("accountService")
+@Scope(ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class AccountServiceImpl implements AccountService {
 
 	@Autowired
